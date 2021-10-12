99299daa5ca9d2814d3746385bb2e07d6d10f3e2
ipekustunboyaci <90289632+ipekustunboyaci@users.noreply.github.com>
2021-10-12 16:42:27 +0200

    Update app.py

--- Assignment1/data_pp_container/app.py
+++ Assignment1/data_pp_container/app.py
@@ -18,4 +18,13 @@ def clean_data(table_name):
     data_cleaner.clean(df)
     return json.dumps({'message': 'data is cleaned'}, sort_keys=False, indent=4), 200
 
+@app.route('/db_preprocessing/<table_name>', methods=['GET'])
+def read_data(table_name):
+    db_api = os.environ['TRAININGDB_API']
+    r = requests.get(db_api)
+    j = r.json()
+    df = pd.DataFrame.from_dict(j)
+    resp = data_cleaner.clean(df)
+    return resp
+
 app.run(host='0.0.0.0', port=5006)
