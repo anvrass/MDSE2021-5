b71940bafebcfaab8f684f5ba1f4aaed4f99f20c
anvrass <68278108+anvrass@users.noreply.github.com>
2021-10-13 14:11:03 +0200

    Fixed(?) format error in output of container 4

--- Assignment1/prediction-cp/resources/predictor.py
+++ Assignment1/prediction-cp/resources/predictor.py
@@ -13,9 +13,8 @@ def predict(dataset):
         #model = load_model(file_path)
         model = pickle.load(open(file_path, 'rb'))
         val_set2 = dataset.copy()
-        result = model.predict(dataset)
-        y_classes = result.argmax(axis=-1)
-        val_set2['class'] = y_classes.tolist()
+        y_result =  model.predict(dataset)
+        val_set2['result'] = y_result.tolist()
         dic = val_set2.to_dict(orient='records')
         return jsonify(dic), 200
     else:
