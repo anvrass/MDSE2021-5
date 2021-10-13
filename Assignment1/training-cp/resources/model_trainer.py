a28f0a0a8ffbc46fc631b53a1338c83e0cd64867
a.rass <anvrass@gmail.com>
2021-10-13 18:48:29 +0200

    intgrated camila's code

--- Assignment1/training-cp/resources/model_trainer.py
+++ Assignment1/training-cp/resources/model_trainer.py
@@ -19,8 +19,37 @@ def train(dataset):
     X = dataset[['Acres', 'Deck', 'GaragCap', 'Patio', 'PkgSpacs', 'Taxes', 'TotBed', 'TotBth', 'TotSqf']]
     y = dataset['SoldPrice']
     X = sm.add_constant(X)
-    
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+    # We now wiull use scikitlearn's train_test_split to split our set into a training and a testing set for our
+    # X and y. I will do this in a way to prepare for a 5-fold cross validation. 
+    # once this is done, we use te train data to train our model and use the test data to see how well our prediction fits
+    # the test data.
+    # We generate a prediction
+
+    # We shall use R_squared as a metric of how well the model fits the test data.
+
+    r2_vals = []
+
+    for i in range(10) :
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+        model = sm.OLS(y_train.astype(float), X_train.astype(float)).fit()
+        model_pred = model.predict(X_test)
+        r2_vals.append(r2(y_test, model_pred))
+
+    print('Mean R-Squared over 10 runs: ' + str(np.mean(r2_vals))) 
+    print("")
+    text_out = {'Mean R-Squared over 10 runs:' :str(np.mean(r2_vals))}
+    #text_out = {model.summary()}
+
+    # Of all variables used in the model, two stand out to be particularly good for price prediction being Taxes and TotSqf
+    # with both their standard error being ~7 and ~11 respectively. they are followed by PkgSpacs, having a standard 
+    # error of ~3728. All other variables have a standard error that ios at least almost twice the standard error of 
+    # PkgSpacs.
+
+    # Using 5-fold cross validation, we tend to average around 0.8 for our R-Squared value.
+    # While close and fairly impressive by itself, it is not accurate enough to beat the R-Squared score
+    # of List Price vs SoldPrice at 0.991
     
     ridge_reg = Ridge(alpha=10, fit_intercept=True)
     ridge_reg.fit(X_train, y_train)
