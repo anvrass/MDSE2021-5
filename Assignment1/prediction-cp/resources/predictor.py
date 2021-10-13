4feb41bc4cd1504555c9dba9c76396cb1b8e58a7
a.rass <anvrass@gmail.com>
2021-10-13 19:42:45 +0200

    updated predictor to hopefully output json dict

--- Assignment1/prediction-cp/resources/predictor.py
+++ Assignment1/prediction-cp/resources/predictor.py
@@ -11,15 +11,11 @@ def predict(dataset):
     model_repo = os.environ['MODEL_REPO']
     if model_repo:
         file_path = os.path.join(model_repo, "model.pkl")
-        #model = load_model(file_path)
         model = pickle.load(open(file_path, 'rb'))
-        #val_set2 = dataset.copy()
+        val_set2 = dataset.copy()
         model_pred = model.predict(dataset)
-        #y_classes = result.argmax(axis=-1)
-        #val_set2['class'] = y_classes.tolist()
-        #dic = result.to_dict(orient='records')
-        #dic = model_pred.to_dict()
-        #text_out = {dic}
-        return jsonify({'message': 'predicted samples'}), 200
+        val_set2['class'] = y_classes.tolist()
+        dic = val_set2.to_dict(orient='records')
+        return jsonify(dic), 200
     else:
         return jsonify({'message': 'MODEL_REPO cannot be found.'}), 200
