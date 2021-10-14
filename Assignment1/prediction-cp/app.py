1eac60e8c8f2a50799b1ea75fea3833a6d47673c
ipekustunboyaci <90289632+ipekustunboyaci@users.noreply.github.com>
2021-10-14 20:49:22 +0200

    Update app.py

--- Assignment1/prediction-cp/app.py
+++ Assignment1/prediction-cp/app.py
@@ -11,11 +11,16 @@ app.config["DEBUG"] = True
 
 @app.route('/prediction-cp/results', methods=['POST'])
 def predict_perf():
+    db_api = os.environ['DB_API']
+    # Make a GET request to training db service to retrieve the prediction data/features.
+    r = requests.get(db_api)
+    j = r.json()
+    df = pd.DataFrame.from_dict(j)
     #training = os.environ['TRAINING']
     #r = requests.get(training)
     # receive the prediction request data as the message body
-    content = request.get_json()
-    df = pd.read_json(json.dumps(content), orient='records')
+    #content = request.get_json()
+    #df = pd.read_json(json.dumps(content), orient='records')
     resp = predictor.predict(df)
     return resp
 
