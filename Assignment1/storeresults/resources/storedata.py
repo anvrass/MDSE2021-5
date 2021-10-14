b977b5160d7b2941150584b73269008b8f9a31da
ipekustunboyaci <90289632+ipekustunboyaci@users.noreply.github.com>
2021-10-14 22:32:21 +0200

    Create storedata.py

---
+++ Assignment1/storeresults/resources/storedata.py
@@ -0,0 +1,22 @@
+import logging
+import os
+
+import pandas as pd
+import numpy as np
+import pickle
+
+from flask import jsonify
+
+
+def store(dataset):
+    df = dataset
+    data_repo = os.environ['DATA_REPO']
+    
+    if data_repo:
+        file_path = os.path.join(data_repo, "predictions.txt")
+        df.save(file_path)
+        logging.info("Saved the predictions to the location : " + data_repo)
+        return jsonify(text_out), 200
+    else:
+        df.save("predictions.txt")
+        return jsonify({'message': 'Predictions are saved locally.'}), 200
