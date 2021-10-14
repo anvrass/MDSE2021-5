78476ebf8e8b708ff1f997e15dcd85e11e5cd072
ipekustunboyaci <90289632+ipekustunboyaci@users.noreply.github.com>
2021-10-14 21:37:12 +0200

    Update predictor.py

--- Assignment1/prediction-cp/resources/predictor.py
+++ Assignment1/prediction-cp/resources/predictor.py
@@ -1,7 +1,6 @@
 import os
 
 from flask import jsonify
-#from keras.models import load_model
 import pickle
 import numpy as np
 import statsmodels.api as sm
@@ -21,15 +20,14 @@ def predict(dataset):
     model_repo = os.environ['MODEL_REPO']
     if model_repo:
         file_path = os.path.join(model_repo, "model.pkl")
-        #model = load_model(file_path)
         model = pickle.load(open(file_path, 'rb'))
+        
         val_set2 = X_test.copy()
         model_pred = model.predict(X_test)
-        #y_classes = result.argmax(axis=-1)
+  
         val_set2['class'] = model_pred
         dic = val_set2.to_dict(orient='records')
-        #dic = model_pred.to_dict()
-        #text_out = {dic}
+
         return jsonify(dic), 200
     else:
         return jsonify({'message': 'MODEL_REPO cannot be found.'}), 200
