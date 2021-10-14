e3dd1afb2a7ab6964e6c1dab2a82986869c66e6b
ipekustunboyaci <90289632+ipekustunboyaci@users.noreply.github.com>
2021-10-14 21:13:45 +0200

    Update predictor.py

--- Assignment1/prediction-cp/resources/predictor.py
+++ Assignment1/prediction-cp/resources/predictor.py
@@ -4,19 +4,23 @@ from flask import jsonify
 #from keras.models import load_model
 import pickle
 import numpy as np
+import statsmodels.api as sm
 
 
 # make prediction
 def predict(dataset):
+    X = dataset[['Acres', 'Deck', 'GaragCap', 'Patio', 'PkgSpacs', 'Taxes', 'TotBed', 'TotBth', 'TotSqf']]
+    y = dataset['SoldPrice']
+    X = sm.add_constant(X)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
     
-    dataset = dataset[['Acres', 'Deck', 'GaragCap', 'Patio', 'PkgSpacs', 'Taxes', 'TotBed', 'TotBth', 'TotSqf']]
     model_repo = os.environ['MODEL_REPO']
     if model_repo:
         file_path = os.path.join(model_repo, "model.pkl")
         #model = load_model(file_path)
         model = pickle.load(open(file_path, 'rb'))
         #val_set2 = dataset.copy()
-        model_pred = model.predict(dataset)
+        model_pred = model.predict(X_test)
         #y_classes = result.argmax(axis=-1)
         #val_set2['class'] = y_classes.tolist()
         #dic = result.to_dict(orient='records')
