c59e559855d44852656f22221e6b7e0219609f13
ipekustunboyaci <90289632+ipekustunboyaci@users.noreply.github.com>
2021-11-28 19:56:47 +0100

    Update app.py

--- Assignment2/BatchPipeline/deployment/dashboard/app.py
+++ Assignment2/BatchPipeline/deployment/dashboard/app.py
@@ -4,7 +4,7 @@ import os
 import flask
 import pandas as pd
 import requests
-from flask import Flask
+from flask import Flask, json, request, Response
 from google.cloud import bigquery
 
 app = Flask(__name__)
@@ -14,15 +14,22 @@ app.config["DEBUG"] = True
 @app.route('/results', methods=['GET'])
 def results():
     client = bigquery.Client(project="de2021-325520")  # use your project id
+    
+    table_ref = client.dataset("a2_dataset").table("products")
+    
+    table = client.get_table(table_ref)
+    
+    df = client.list_rows(table).to_dataframe()
+    resp = Response(df.to_json(orient='records'), status=200, mimetype='application/json')
+    return resp
+    #QUERY = (
+    #    'SELECT * FROM `de2021-325520.a2_dataset.products` LIMIT 100')  # use the correct project id, etc.
+    #query_job = client.query(QUERY)  # API request
+    #rows = query_job.result()
 
-    QUERY = (
-        'SELECT * FROM `de2021-325520.a2_dataset.products` LIMIT 100')  # use the correct project id, etc.
-    query_job = client.query(QUERY)  # API request
-    rows = query_job.result()
-
-    for row in rows:
-        output = "Record: " + str(row)
-    return output
+    #for row in rows:
+    #    output = "Record: " + str(row)
+    #return output
 
 
 app.run(host='0.0.0.0',port=5000)
