84bf41a4fef787c0fd1a42513960fec24fbb328a
ipekustunboyaci <90289632+ipekustunboyaci@users.noreply.github.com>
2021-11-28 19:55:56 +0100

    Add files via upload

---
+++ Assignment2/BatchPipeline/deployment/dashboard/app.py
@@ -0,0 +1,28 @@
+import concurrent.futures
+import os
+
+import flask
+import pandas as pd
+import requests
+from flask import Flask
+from google.cloud import bigquery
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/results', methods=['GET'])
+def results():
+    client = bigquery.Client(project="de2021-325520")  # use your project id
+
+    QUERY = (
+        'SELECT * FROM `de2021-325520.a2_dataset.products` LIMIT 100')  # use the correct project id, etc.
+    query_job = client.query(QUERY)  # API request
+    rows = query_job.result()
+
+    for row in rows:
+        output = "Record: " + str(row)
+    return output
+
+
+app.run(host='0.0.0.0',port=5000)
