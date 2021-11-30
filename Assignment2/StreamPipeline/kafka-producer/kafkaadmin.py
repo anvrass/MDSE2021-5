13c1df9774ee8113676315d71835e43dab395534
ipekustunboyaci <90289632+ipekustunboyaci@users.noreply.github.com>
2021-11-30 23:09:04 +0100

    Update kafkaadmin.py

--- Assignment2/StreamPipeline/kafka-producer/kafkaadmin.py
+++ Assignment2/StreamPipeline/kafka-producer/kafkaadmin.py
@@ -1,7 +1,7 @@
 from kafka.admin import KafkaAdminClient, NewTopic
 
 def delete_topics(admin):
-    admin.delete_topics(topics=['game', 'avg_score'])
+    admin.delete_topics(topics=['reddit', 'corr_score'])
 
 
 def create_topics(admin, topic_list):
@@ -9,9 +9,9 @@ def create_topics(admin, topic_list):
 
 
 if __name__ == '__main__':
-    admin_client = KafkaAdminClient(bootstrap_servers="34.133.77.8:9092",
+    admin_client = KafkaAdminClient(bootstrap_servers="34.68.123.176:9092",
                                     client_id='ipek')  # use your VM's external IP Here!
-    topic_list = [NewTopic(name="game", num_partitions=1, replication_factor=1),
-                  NewTopic(name="avg_score", num_partitions=1, replication_factor=1),
-                  NewTopic(name="reddit", num_partitions=1, replication_factor=1)]
+    topic_list = [NewTopic(name="reddit", num_partitions=1, replication_factor=1),
+                  NewTopic(name="corr_score", num_partitions=1, replication_factor=1)]
+    delete_topics(admin_client)
     create_topics(admin_client, topic_list)
